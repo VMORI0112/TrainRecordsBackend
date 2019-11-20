cb63c68afd13f4fb4800591d1c244469ab6ad7bd
VMORI0112 <victorfernan_moriganador@hotmail.com>
2019-11-20 18:01:45 +0000

    update model

--- src/models.py
+++ src/models.py
@@ -23,9 +23,44 @@ class Users(db.Model):
             "avatar": self.avatar,
         }
 
-class AircraftTrainRecords(db.Model):
+class TrainData(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    coursenumber = db.Column(db.Integer)
-    hasrecu = db.Column(db.String(1)) 
-    
+    courseNumber = db.Column(db.Integer)
+    hasRecu = db.Column(db.String(1)) 
+    descriptionName = db.Column(db.String(60)
+    dateAtten = db.Column(db.Datetime)
+    ceCo = db.Column(db.String(12))
+    trainingGroup = db.Column(db.String(30))
+    name = db.Column(db.String(40))
+    hours = db.Column(db.String(3))
+    days = db.Column(db.Decimal(7, 2))
+    sta = db.Column(db.String(1))
+    a&P = db.Column(db.Integer)
+    insIni = db.Column(db.String(3))
+    recurrent = db.Column(db.String(1)) 
+    oneYearExpire = db.Column(db.Datetime)
+    twoYearExpire = db.Column(db.Datetime)
+    threeYearExpire = db.Column(db.Datetime)
+    fourYearExpire = db.Column(db.Datetime)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "CourseNumber": self.courseNumber,
+            "HasRecu": self.hasRecu,
+            "DescriptionName": self.descriptionName,
+            "DateAtten": self.dateAtten,
+            "CostCenter": self.ceCo,
+            "TrainingGroup": self.trainingGroup,
+            "Name": self.name,
+            "Status": self.sta,
+            "A&PLicense": self.a&P,
+            "InstrutorInitials": self.insIni,
+            "CourseRecurrence": self.recurrent,
+            "TwoYearExpire": self.TwoYearExpire,
+            "ThreeYearExpire": self.ThreeYearExpire,
+        }
+
+
+
 
