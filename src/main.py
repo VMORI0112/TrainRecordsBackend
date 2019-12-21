78af7343736913cfb38db642bc4d5556355abfc2
Samir Benzada <samirbenzada@gmail.com>
2019-12-21 17:42:04 +0000

    update

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
-from utils import APIException, generate_sitemap
+from utils import APIException, generate_sitemap, sha256
 from models import db, Users, CourseTable, TrainingData, TrainData
 from flask_jwt_simple import JWTManager, jwt_required, create_jwt
 
@@ -45,8 +45,63 @@ def handle_users():
 
     return "Invalid Method", 404
 
+@app.route('/login', methods=['POST'])
+def handle_login():
+
+    body = request.get_json()
+
+    user = Users.query.filter_by(email=body['email'], password=sha256(body['password'])).first()
+
+    if not user:
+        return 'User not found', 404
+
+    return jsonify({
+              'token': create_jwt(identity=1),
+              'id': user.id,
+              'email': user.email,
+              'firstname': user.firstname,
+              'lastname': user.lastname,
+              'avatar': user.avatar,
+              'wallet': user.wallet
+              })
+
+@app.route('/register', methods=['POST'])
+def handle_register():
+
+    body = request.get_json()
+
+    if body is None:
+        raise APIException("You need to specify the request body as a json object", status_code=400)
+    if 'firstname' not in body and 'lastname' not in body:
+        raise APIException("You need to specify the first name and last name", status_code=400)
+    if 'password' not in body and 'email' not in body:
+        raise APIException("You need to specify the password and email", status_code=400)
+    if 'firstname' not in body:
+        raise APIException('You need to specify the first name', status_code=400)
+    if 'lastname' not in body:
+        raise APIException('You need to specify the last name', status_code=400)
+    if 'password' not in body:
+        raise APIException('You need to specify the password', status_code=400)
+    if 'email' not in body:
+        raise APIException('You need to specify the email', status_code=400)
+
+
+
+    db.session.add(Users(
+        email = body['email'],
+        firstname = body['firstname'],
+        lastname = body['lastname'],
+        password = sha256(body['password'])
+    ))
+    db.session.commit()
+
+    return jsonify({
+        'register': 'success',
+        'msg': 'Successfully Registered'
+    })
+
 @app.route('/coursetable', methods=['GET'])
-def get_records():
+def get_coursetable():
     if request.method == 'GET':
         records = CourseTable.query.all()
 
@@ -58,7 +113,7 @@ def get_records():
     return "Invalid Method", 404    
 
 @app.route('/trainingdata', methods=['GET'])
-def get_records():
+def get_trainingdata():
     if request.method == 'GET':
         records = TrainingData.query.all()
 
@@ -70,7 +125,7 @@ def get_records():
     return "Invalid Method", 404   
 
 @app.route('/traindata', methods=['GET'])
-def get_records():
+def get_traindata():
     if request.method == 'GET':
         records = TrainData.query.all()
 
--- src/models.py
+++ src/models.py
@@ -23,89 +23,94 @@ class Users(db.Model):
             "avatar": self.avatar,
         }
 class CourseTable(db.Model):
-    courseNumber = db.Column(db.Integer)
+    courseNumber = db.Column(db.Integer, primary_key=True)
     category = db.Column(db.String(30)) 
-    descriptionName = db.Column(db.String(60)
+    descriptionName = db.Column(db.String(60))
     dataType = db.Column(db.String(10))
     recurrent = db.Column(db.String(1))
     hours = db.Column(db.String(3))
-    days = db.Column(db.Decimal(7, 2))
-    hours = db.Column(db.String(3))
+    days = db.Column(db.Float(7, 2))
     companyTrainer = db.Column(db.String(6))
     sta = db.Column(db.String(1))
    
     def serialize(self):
         return {
-            "CourseNumber": self.courseNumber,
-            "Category": self.category,
-            "DescriptionName": self.descriptionName,
-            "DataType": self.dataType,
-            "CourseRecurrence": self.recurrent,
-            "HoursCourse ": self.hours,
-            "DaysCourse": self.days,
-            "CompanyTrainer": self.companyTrainer,
-            "Status": self.sta,
+            "courseNumber": self.courseNumber,
+            "category": self.category,
+            "descriptionName": self.descriptionName,
+            "dataType": self.dataType,
+            "recurrent": self.recurrent,
+            "hours ": self.hours,
+            "days": self.days,
+            "companyTrainer": self.companyTrainer,
+            "sta": self.sta,
         }
 class TrainingData(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     courseNumber = db.Column(db.Integer)
     hasRecu = db.Column(db.String(1)) 
-    dateAtten = db.Column(db.Datetime)
+    dateAtten = db.Column(db.DateTime)
     ceCo = db.Column(db.String(12))
     trainingGroup = db.Column(db.String(30))
     name = db.Column(db.String(40))
     sta = db.Column(db.String(1))
-    a&P = db.Column(db.Integer)
+    anp = db.Column(db.Integer)
+    InstrutorInitials = db.Column(db.String(12))
     
     def serialize(self):
         return {
             "id": self.id,
             "CourseNumber": self.courseNumber,
-            "HasRecurrent": self.hasRecu,
+            "hasRecu": self.hasRecu,
             "DateAtten": self.dateAtten,
-            "CostCenter": self.ceCo,
+            "ceCo": self.ceCo,
             "TrainingGroup": self.trainingGroup,
             "Name": self.name,
-            "Status": self.sta,
-            "A&PLicense": self.a&P,
-            "InstrutorInitials": self.insIni,
+            "sta": self.sta,
+            "anp": self.anp,
+            "InstrutorInitials": self.InstrutorInitials
         }
+
 class TrainData(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     courseNumber = db.Column(db.Integer)
     hasRecu = db.Column(db.String(1)) 
-    descriptionName = db.Column(db.String(60)
-    dateAtten = db.Column(db.Datetime)
+    descriptionName = db.Column(db.String(60))
+    dateAtten = db.Column(db.DateTime)
     ceCo = db.Column(db.String(12))
     trainingGroup = db.Column(db.String(30))
     name = db.Column(db.String(40))
     hours = db.Column(db.String(3))
-    days = db.Column(db.Decimal(7, 2))
+    days = db.Column(db.Float(7, 2))
     sta = db.Column(db.String(1))
-    a&P = db.Column(db.Integer)
+    anp = db.Column(db.Integer)
     insIni = db.Column(db.String(3))
     recurrent = db.Column(db.String(1)) 
-    oneYearExpire = db.Column(db.Datetime)
-    twoYearExpire = db.Column(db.Datetime)
-    threeYearExpire = db.Column(db.Datetime)
-    fourYearExpire = db.Column(db.Datetime)
+    oneYearExpire = db.Column(db.DateTime)
+    twoYearExpire = db.Column(db.DateTime)
+    threeYearExpire = db.Column(db.DateTime)
+    fourYearExpire = db.Column(db.DateTime)
 
     def serialize(self):
         return {
-            "id": self.id,
-            "CourseNumber": self.courseNumber,
-            "HasRecurrent": self.hasRecu,
-            "DescriptionName": self.descriptionName,
-            "DateAtten": self.dateAtten,
-            "CostCenter": self.ceCo,
-            "TrainingGroup": self.trainingGroup,
-            "Name": self.name,
-            "Status": self.sta,
-            "A&PLicense": self.a&P,
-            "InstrutorInitials": self.insIni,
-            "CourseRecurrence": self.recurrent,
-            "TwoYearExpire": self.TwoYearExpire,
-            "ThreeYearExpire": self.ThreeYearExpire,
+            "id" : self.id,
+            "courseNumber" : self.courseNumber,
+            "hasRecu" : self.hasRecu,
+            "descriptionName" : self.descriptionName,
+            "dateAtten" : self.dateAtten,
+            "ceCo" : self.ceCo,
+            "trainingGroup" : self.trainingGroup,
+            "name" : self.name,
+            "hours" : self.hours,
+            "days" : self.days,
+            "sta" : self.sta,
+            "anp" : self.anp,
+            "insIni" : self.insIni,
+            "recurrent" : self.recurrent,
+            "oneYearExpire" : self.oneYearExpire,
+            "twoYearExpire" : self.twoYearExpire,
+            "threeYearExpire" : self.threeYearExpire,
+            "fourYearExpire" : self.fourYearExpire
         }
 
 
